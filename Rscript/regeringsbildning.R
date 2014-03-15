39f0cc761bbb0b0b85864ef7a03037a1439f3df5
LCHansson <love.hansson@gmail.com>
2014-03-15 12:08:53 +0100

    Implemented function findGovernment()

--- Rscript/regeringsbildning.R
+++ Rscript/regeringsbildning.R
@@ -48,7 +48,7 @@ findSeats <- function(voteList, threshold = 4, seats = 349) {
   
   for (i in 1:seats) {
     seatWinner = which.max(sapply(names(partiResults), function(parti) {
-      result = partiResults[[parti]]/(1 + mandateList[[parti]])
+      result = partiResults[[parti]]/(1 + 2*mandateList[[parti]])
       return(result)
     }))
     mandateList[names(seatWinner)] = mandateList[[names(seatWinner)]] + 1
@@ -63,7 +63,8 @@ findSeats <- function(voteList, threshold = 4, seats = 349) {
 #' The basic problem is this: Given a strength balance between political parties,
 #' decide which potential party coalition might form a government.
 #' 
-#' The function takes a formatted list of 
+#' The function takes a formatted list of seats in parliament and finds a government
+#' by following the steps outlined below.
 #' 
 #' We assume the following premises:
 #' * All parties have a specified list of which parties they _can_ cooperate with
@@ -73,14 +74,72 @@ findSeats <- function(voteList, threshold = 4, seats = 349) {
 #' for potential coalition partners and decides whether they can form a coalition
 #' with them.
 #' * If the biggest party fails at finding a coalition, the second biggest party
-#' tries to do the same thing.
-#' * If no party with a vote share above 20% manages to form a coalition, the 
+#' gets to choose a coalition.
+#' * If none of the two biggest parties manages to form a coalition, the 
 #' coalition formation is considered to have failed.
 #' * Parties stop looking for coalition partners once they have reached >50% of
 #' the mandate share.
 
-findGovernment <- function(mandateList) {
+findGovernment <- function(seatList) {
   
+  # We begin by implementing a naïve method by identifying potential coalitions as
+  # ordered lists. Coalition preference order is determined by 
+  coalitionPreferences <- list(
+    S = c("MP","V","FP"),
+    V = c("S","MP","M","FP"),
+    MP = c("S","V","FP","M"),
+    M = c("FP","KD","C","MP"),
+    C = c("M","S","FP","MP"),
+    KD = c("M","FP","C","S"),
+    FP = c("M","C","S","MP"),
+    SD = c("M","KD","C","S")
+  )
+  
+  # First we need to determine the majority threshold.
+  numSeats = sum(sapply(seatList, sum))
+  majorityThreshhold = floor(numSeats/2) + 1
+  
+  # The biggest party is fetched from the seat List
+  biggestParty = names(which.max(seatList))
+  
+  # The biggest party makes the first attempt at forming a coalition as determined by
+  # its coalition preferences
+  coalition = list()
+  
+  for (partner in c(biggestParty, coalitionPreferences[[biggestParty]])) {
+    coalition = append(coalition, seatList[partner])
+    coalitionSeats = sum(unlist(coalition))
+    if (coalitionSeats >= majorityThreshhold)
+      break
+  }
+  
+  # If there is a successful coalition, we have a winner!
+  # Otherwise, repeat the procedure for the second biggest party.
+  if (coalitionSeats >= majorityThreshhold) {
+    return(coalition)
+  } else {
+    
+    secondParty = names(which.max(seatList[names(seatList) != secondParty]))
+    
+    coalition = list()
+    
+    for (partner in c(secondParty, coalitionPreferences[[secondParty]])) {
+      coalition = append(coalition, seatList[partner])
+      coalitionSeats = sum(unlist(coalition))
+      if (coalitionSeats >= majorityThreshhold)
+        break
+    }
+  }
+  
+  # If the two biggest parties have both failed at forming a majority coalition,
+  # the coalition formation process is considered to have failed. (This should not
+  # be possible in the naïve implementation.)
+  if (coalitionSeats >= majorityThreshhold) {
+    return(coalition)
+  } else {
+    warning ("Could not find a successful government coalition.")
+    return(NULL)
+  }
 }
 
 
