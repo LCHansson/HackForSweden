2a46e2080b2894d7359018570ec2509f53dfb0f9
Jonathan Hise Kaldma <info@hisekaldma.com>
2014-03-15 17:44:32 +0100

    Use mapbox geocoder

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,12 +1,19 @@
 var startCoord = [63, 18];
 var startZoom = 5;
 var resultZoom = 15;
-var map = L.mapbox.map('map', 'hisekaldma.hh4jiokf')
+
+var map = L.mapbox.map('map', 'hisekaldma.hh4jiokf', {
+        tileLayer: {
+            detectRetina: true
+        }
+    })
     .setView(startCoord, startZoom);
+
+var geocoder = L.mapbox.geocoder('hisekaldma.hh4jiokf');
 
 $(function() {
     $('#start-view button').click(function() {
-        showResults($('#start-view input').val());
+        showPosition($('#start-view input').val());
     });
 });
 
@@ -21,4 +28,29 @@ function showResults(searchString) {
         $('#result-view strong').text(name);
         map.setView(coordinates, resultZoom, { animate: true });
     });
+}
+
+function showPosition(searchString) {
+    geocoder.query(searchString + ' Sweden', function(error, data) {
+        var result = findBestResult(data.results);
+        var coordinates = [result[0].lat, result[0].lon];
+
+        $('#start-view').hide();
+        $('#result-view').show();
+        map.setView(coordinates, resultZoom, { animate: true });
+    });
+}
+
+function findBestResult(results) {
+    var cities = ['Stockholm', 'Goteborg', 'Malmo'];
+
+    for (var i = 0; i < cities.length; i++) {
+        for (var j = 0; j < results.length; j++) {
+            if (results[j][1].name == cities[i]) {
+                return results[j];
+            }
+        }
+    }
+
+    return results[0];
 }
