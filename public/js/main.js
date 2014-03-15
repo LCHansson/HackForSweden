960c8063ff7297fad997eb7d695ce48a09a24426
Jonathan Hise Kaldma <info@hisekaldma.com>
2014-03-15 11:02:16 +0100

    We gots map!

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,6 @@
+var map = L.mapbox.map('map', 'hisekaldma.hh4jiokf')
+    .setView([40, -74.50], 9);
+    
 $(function() {
     $('#start-view button').click(function() {
         $('#start-view').hide();
